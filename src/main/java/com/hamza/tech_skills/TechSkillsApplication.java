4703bbc39da352ccb4e16dc35e7e928bb9307893
TOUHARDJI HAMZA p1805596 <hamza.touhardji@etu.univ-lyon1.fr>
2020-12-12 13:43:18 +0100

    initial commit

---
+++ src/main/java/com/hamza/tech_skills/TechSkillsApplication.java
@@ -0,0 +1,13 @@
+package com.hamza.tech_skills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSkillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSkillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamza/tech_skills/TechSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamza.tech_skills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
