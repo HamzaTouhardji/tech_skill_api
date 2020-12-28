d98aeba3539925cc0c07312f47eff8d3cd7c2937
Hamza Touhardji <h.touhardji@gmail.com>
2020-12-28 15:16:07 +0100

    bdd connexion + add user class

---
+++ src/main/java/com/hamza/tech_skills/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.hamza.tech_skills.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hamza.tech_skills.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
