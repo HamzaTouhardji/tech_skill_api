9fdddd17d8f89bd1d0b7aa597b88a96299ca04ef
philippe.aubertin@axopen.com <Axopen01>
2021-02-25 13:58:56 +0100

    Add GraalVM

--- src/test/java/com/hamza/tech_skills/TechSkillsApplicationTests.java
+++ src/test/java/com/hamza/tech_skills/TechSkillsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.hamza.tech_skills;
 
+import org.graalvm.polyglot.Value;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.graalvm.polyglot.Context;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 @SpringBootTest
 class TechSkillsApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		Context context = Context.create("");
+		Value js = context.eval("js", "console.log(toot)");
+
 	}
 
 }
